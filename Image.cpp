fe24acc976860fc434001da97769bb2ad26a7e4a
Pietrius42 <114315228+Pietrius42@users.noreply.github.com>
2023-04-27 16:58:59 +0100

    Update Image.cpp

--- Image.cpp
+++ Image.cpp
@@ -4,17 +4,26 @@ namespace prog
 {
   Image::Image(int w, int h, const Color &fill)
   {
+    _width=w;
+    _height=h;
+    Color used_fill;
+    used_fill.Color(fill)
+    for (int row=0;row<h ;row++){
+      for(int collum=0; collum<w ;collum++){
+        image_matrix.push_back(used_fill);
+      }
+    }
   }
   Image::~Image()
   {
   }
   int Image::width() const
   {
-    return -1;
+    return _width;
   }
   int Image::height() const
   {
-    return -1;
+    return _height;
   }
 
   // TODO: remove this DUMMY_color variable once you have appropriate fields for representing image pixels.
