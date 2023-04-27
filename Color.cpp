4c699add2f453eceb42fda319206601886a60406
Pietrius42 <114315228+Pietrius42@users.noreply.github.com>
2023-04-27 15:54:09 +0100

    Update Color.cpp

--- Color.cpp
+++ Color.cpp
@@ -1,25 +1,68 @@
 #include "Color.hpp"
 
 namespace prog {
-    Color::Color() {
+    //constructors
+    Color::Color() 
+        {
+         //set RGB to black (0,0,0)
+         _red=0;
+        _green=0;
+        _blue=0;
+        }
+    Color::Color(const Color& other) 
+        {
+        //copy constructor
+        this->_red=other._red;
+        this->_green=other._green;
+        this->_blue=other._blue;
+        }
+    Color::Color(rgb_value red, rgb_value green, rgb_value blue) 
+        {
+        //constructer with specified values
+        _red = red;
+        _green = green;
+        _blue = blue;
+        }
 
-    }
-    Color::Color(const Color& other) {
+    // get member values
+    rgb_value Color::red() const
+        {
+        //get current red
+        return _red;
+        }
+    rgb_value& Color::red()
+        {
+        //get mutable red (breaks encapsulation)
+        return _red;
+        }
+        
 
-    }
-    Color::Color(rgb_value red, rgb_value green, rgb_value blue) {
+    rgb_value Color::green() const 
+        {
+        //get current green
+        return _green;
+        }
+    rgb_value& Color::green()
+        {
+        //get mutable green (breaks encapsulation)
+        return _green;
+        }
 
-    }
-    rgb_value Color::red() const {
-        return 0;
-    }
-    rgb_value Color::green() const {
-        return 0;
-    }
-    rgb_value Color::blue() const {
-        return 0;
-    }
 
+    rgb_value Color::blue() const 
+        {
+        //get current blue
+        return _blue;
+        }
+
+    rgb_value& Color::blue()
+        {
+        //get mutable blue (breaks encapsulation)
+        return _blue;
+        }
+
+
+/*
     // TODO: remove this DUMMY variable once you have appropriate fields for representing colors.
     rgb_value DUMMY_rgb_value = 0; 
 
@@ -32,4 +75,5 @@ namespace prog {
     rgb_value& Color::blue()  {
       return DUMMY_rgb_value;
     }
+    */
 }
