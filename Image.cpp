13d5c683522fdf01eaf18fe82dffc10336bc6ee7
Pietrius42 <114315228+Pietrius42@users.noreply.github.com>
2023-04-29 22:30:35 +0100

    Update Image.cpp

--- Image.cpp
+++ Image.cpp
@@ -1,41 +1,45 @@
 #include "Image.hpp"
+#include "Color.hpp"
+#include <iostream>
+using namespace std;
+
 
 namespace prog
 {
   Image::Image(int w, int h, const Color &fill)
   {
-    _width=w;
-    _height=h;
-    Color used_fill;
-    used_fill.Color(fill)
-    for (int row=0;row<h ;row++){
-      for(int collum=0; collum<w ;collum++){
-        image_matrix.push_back(used_fill);
-      }
+    _width  = w;
+    _height = h;
+
+    for(int row = 0; row < _height ; row++)
+    {
+      vector<Color> new_row(_width, fill);
+      image_matrix.push_back(new_row);
+
     }
   }
+
   Image::~Image()
   {
   }
+
   int Image::width() const
   {
     return _width;
   }
+
   int Image::height() const
   {
     return _height;
   }
 
-  // TODO: remove this DUMMY_color variable once you have appropriate fields for representing image pixels.
-  Color DUMMY_color;
-
-  Color& Image::at(int x, int y)
+  Color& Image:: at(int x, int y)
   {
-    return DUMMY_color;
+    return image_matrix[y][x];
   }
 
-  const Color& Image::at(int x, int y) const
+    const Color& Image::at(int x, int y) const
   {
-    return DUMMY_color;
+    return image_matrix[y][x];
   }
 }
