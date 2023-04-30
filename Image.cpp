58ed99e54632e5696313d23fc3f1ab302f57bc37
Pietrius42 <114315228+Pietrius42@users.noreply.github.com>
2023-04-30 18:31:54 +0100

    Update Image.cpp

--- Image.cpp
+++ Image.cpp
@@ -1,30 +1,50 @@
 #include "Image.hpp"
+#include "Color.hpp"
 #include <iostream>
 using namespace std;
 
 
 namespace prog
 {
+  //constructor
   Image::Image(int w, int h, const Color &fill)
   {
-    _width=w;
-    _height=h;
-    Color used_fill = fill;
-    for(int row = 0; row < h ; row++){
-      image_matrix.push_back(vector<Color>());
-      for(int collum = 0; collum < w ;collum++){
-        image_matrix[row].push_back(used_fill);
-      }
+    _width  = w;
+    _height = h;
+    vector<Color> new_row(_width, fill); //define the row
+    for(int row = 0; row < _height ; row++)
+    {
+      image_matrix.push_back(new_row); //fill the matrix row by row
     }
   }
-  Image::~Image(){
+  //unused destructor
+  Image::~Image()
+  {
   }
+
+  //get with
   int Image::width() const
   {
     return _width;
   }
+
+  //get height
   int Image::height() const
   {
     return _height;
+  }
+
+  //get reference to a pixel (braks encapsulation)
+  Color& Image:: at(int x, int y)
+  {
+    return image_matrix[y][x]; 
+    //the matrix is defined row by row therefore the 'Y' is used as index before 'X'
+  }
+
+  //get values of a pixel 
+  const Color& Image::at(int x, int y) const
+  {
+    return image_matrix[y][x];
+    //the matrix is defined row by row therefore the 'Y' is used as index before 'X'
   }
 }
