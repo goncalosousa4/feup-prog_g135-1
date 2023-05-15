9ef89496a1a5ca09499b0152c2b7730e874163ff
Pietrius42 <114315228+Pietrius42@users.noreply.github.com>
2023-05-16 00:22:05 +0100

    Update Image.hpp

--- Image.hpp
+++ Image.hpp
@@ -13,12 +13,14 @@ namespace prog
     int _width, _height;
     vector<vector <Color>> image_matrix;
   public:
-    Image(int w, int h, const Color &fill = {255, 255, 255}); 
+    Image(int w, int h, const Color &fill = {255, 255, 255});
+    Image(int w, int h, const vector<vector <Color>> manual_image);
     ~Image();                                                 
     int width() const;                                        
     int height() const;                                       
     Color &at(int x, int y);                                  
     const Color &at(int x, int y) const;
+    vector<Color>& get_row(int position_of__row);
   };
 }
 #endif
